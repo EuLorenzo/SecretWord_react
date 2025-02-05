0b8b178b546ba60d320cd1aae439835e6ca5db77
euLorenzo <lorenzo.frinhani@gmail.com>
2025-02-05 10:06:16 -0300

    first commit

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Secret Word</h1>
+      <p>Clique no botão abaixo para começar a jogar</p>
+      <Link
+        className={styles.link}
+        children={"Começar a jogar!"}
+        to={"/game"}
+      />
+    </div>
+  );
+};
+
+export default Home;
